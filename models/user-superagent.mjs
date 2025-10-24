eaeea704977c6022deeefb41d697843541f6398b
Usman Ghani <usman.dev24@gmail.com>
2025-10-24 08:47:18 +0500

    feat: user api add

---
+++ models/user-superagent.mjs
@@ -0,0 +1,90 @@
+import { default as request } from "superagent";
+import * as util from "node:util";
+import * as url from "node:url";
+import debug from "debug";
+
+const URL = url.URL;
+const log = debug("notes:users-superagent");
+const error = debug('notes:error-superagent');
+
+var authid = 'them';
+var authcode = 'D4ED43C0-8BD6-4FE2-B358-7C0E230D11EF';
+
+function reqURL(path) {
+  const requrl = new URL(process.env.USER_SERVICE_URL);
+  requrl.pathname = path
+  return requrl.toString()
+}
+
+export async function create(username, password,
+  provider, familyName, givenName, middleName,
+  emails, photos) {
+  const res = await request.post(reqURL('/create-user'))
+    .send({
+      username, password,
+      provider, familyName, givenName, middleName,
+      emails, photos
+    })
+    .set("Content-type", "application/json")
+    .set("Accept", "application/json")
+    .auth(authid, authcode);
+  return res.body
+}
+
+export async function update(username, password,
+  provider, familyName, givenName, middleName,
+  emails, photos) {
+  const res = await request.post(reqURL(`/update-user/${username}`))
+    .send({
+      username, password,
+      provider, familyName, givenName, middleName,
+      emails, photos
+    })
+    .set("Content-type", "application/json")
+    .set("Accept", "application/json")
+    .auth(authid, authcode);
+  return res.body
+}
+
+export async function findOrCreate(profile) {
+  var res = await request
+    .post(reqURL('/find-or-create'))
+    .send({
+      username: profile.id, password: profile.password,
+      provider: profile.provider,
+      familyName: profile.familyName,
+      givenName: profile.givenName,
+      middleName: profile.middleName,
+      emails: profile.emails, photos: profile.photos
+    }).set('Content-Type', 'application/json')
+    .set('Acccept', 'application/json')
+    .auth(authid, authcode);
+  return res.body;
+}
+export async function passwordCheck(username, password) {
+  const res = await request.post(reqURL(`/password-check`))
+    .send({ username, password })
+    .set("Content-type", "application/json")
+    .set("Accept", "application/json")
+    .auth(authid, authcode);
+  return res.body;
+}
+
+export async function find(username) {
+  const res = await request.get(reqURL(`/find/${username}`))
+    .set("Content-type", "application/json")
+    .set("Accept", "application/json")
+    .auth(authid, authcode);
+  return res.body;
+}
+
+export async function list() {
+  const res = await request.get(reqURL(`/list`))
+    .set("Content-type", "application/json")
+    .set("Accept", "application/json")
+    .auth(authid, authcode);
+  return res.body;
+}
+
+
+
