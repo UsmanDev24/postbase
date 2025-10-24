04a66ab0901542fcf8e429149f5b669864326a3c
Usman Ghani <usman.dev24@gmail.com>
2025-10-24 12:23:32 +0500

    feat: users added

--- routes/notes.mjs
+++ routes/notes.mjs
@@ -1,22 +1,24 @@
 import { default as express } from "express";
 import { NotesStore as notes } from "../models/notes-store.mjs";
 import { default as DBG } from "debug";
+import { ensureAuthenticated } from "./users.mjs";
 
 const debug = DBG('notes:routs_notes.mjs')
 const dbgerror = DBG('notes:error')
 export const router = express.Router();
 
 //Add Notes.
-router.get('/add', (req, res, next) => {
+router.get('/add', ensureAuthenticated, (req, res, next) => {
   res.render('noteedit', {
     title: "Add a Note",
     docreate: true,
     notekey: '',
-    note: undefined
+    note: undefined,
+    user: req.user? req.user: undefined
   })
 });
 //save Note (update)
-router.post('/save', async (req, res, next) => {
+router.post('/save', ensureAuthenticated, async (req, res, next) => {
   try {
     let note;
     let notekey = req.body.notekey;
@@ -38,19 +40,21 @@ router.get('/view', async (req, res, next) => {
     let note = await notes.read(req.query.key);
     res.render('noteview', {
       title: note ? note.title : "",
-      notekey: req.query.key, note: note
+      notekey: req.query.key, note: note,
+      user: req.user? req.user: undefined
     })
   } catch (err) { next(err) }
 })
 
 //Edit note (update)
-router.get('/edit', async (req, res, next) => {
+router.get('/edit', ensureAuthenticated, async (req, res, next) => {
   try {
     let note = await notes.read(req.query.key);
     res.render('noteedit', {
       title: note ? ("Edit "+note.title) : "Add a Note",
       docreate : false,
-      notekey: req.query.key, note: note
+      notekey: req.query.key, note: note,
+      user: req.user? req.user: undefined
     })
   } catch (err) {
     next(err)
@@ -58,19 +62,20 @@ router.get('/edit', async (req, res, next) => {
 })
 //TO delelete notes 
 
-router.get('/destroy', async (req, res, next) => {
+router.get('/destroy', ensureAuthenticated, async (req, res, next) => {
   try {
     let note = await notes.read(req.query.key);
     res.render('notedestroy', {
       title: note ? note.title : "",
-      notekey: req.query.key, note: note
+      notekey: req.query.key, note: note,
+      user: req.user? req.user: undefined
     })
   } catch (err) {
     next(err)
   }
 })
 
-router.post('/destroy/confirm', async (req, res, next) => {
+router.post('/destroy/confirm',ensureAuthenticated, async (req, res, next) => {
   try { 
     await notes.destroy(req.body.notekey);
     res.redirect('/');
