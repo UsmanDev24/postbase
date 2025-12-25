eb2ead1f4b0934417d6fc7cb845064b425b74612
Usman Ghani <usman.dev24@gmail.com>
2025-12-25 19:19:58 +0500

    manual ws user authetication add

---
+++ models/ws-session.mjs
@@ -0,0 +1,34 @@
+import * as crypto from 'crypto';
+import { getSessionData } from './prisma-session.mjs';
+import { find as findUser } from './user-superagent.mjs';
+import { sessionCookieName } from '../app.mjs';
+export async function wsSession(rawCookies) {
+  const sessionEntry = rawCookies
+        .split(";")
+        .map(c => c.trim())
+        .find(c => c.startsWith(`${sessionCookieName}=`));
+
+    if (!sessionEntry) {
+        return;
+    }
+
+    const encodedValue = sessionEntry.split("=")[1];
+    const decodedValue = decodeURIComponent(encodedValue); 
+
+    const [prefixedPayload, signature] = decodedValue.split(".");
+    const payload = prefixedPayload.startsWith("s:")
+        ? prefixedPayload.slice(2)
+        : prefixedPayload;
+    console.log(payload, signature)
+
+    const expectedSignature = crypto.createHmac("sha256", process.env.SESSION_COOKIE_SECRET)
+                                    .update(payload)
+                                    .digest("base64");
+    if (expectedSignature.replace("=", "") === signature) {
+        let sessionData = await getSessionData(payload)
+        const username = sessionData.sess.passport.user;
+        const fullUser = await findUser(username);
+        const user = {username: fullUser.username, id: fullUser.id, displayName: fullUser.displayName}
+        return user;
+    }
+}
